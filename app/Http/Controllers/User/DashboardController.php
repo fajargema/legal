3e1ee1045ef6c2f72029d8703636dc82db556f67
Fajar Gema <fgemar72@gmail.com>
2024-07-15 00:15:35 +0700

    fix: dashboard user dan admin

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Legal;
+use App\Models\Residence;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('pages.user.index');
+        $count_residence = Residence::count();
+        $count_legal = Legal::count();
+        $legal = Legal::with('user', 'residence')->limit(5)->get();
+
+        return view('pages.user.index', compact('count_residence', 'count_legal', 'legal'));
     }
 }
